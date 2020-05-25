b4c2327da56e91931b3511b331aad54061214360
prachi-natu <prachinatuproject@gmail.com>
2020-05-25 14:22:45 +0530

    inverse of matrix

--- ProgrammingAssignment2-master/cachematrix.R
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  cache <- NULL
+  setMatrix <- function(newValue) {
+    x <<- newValue
+    cache <<- NULL
 
+  }
+  getMatrix <- function() {
+    x
+  }
+  cacheInverse <- function(solve) {
+    cache <<- solve
+  }
+  getInverse <- function() {
+    cache
+  }
+  list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
 }
 
 
@@ -12,4 +27,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  # if a cached value exists return it
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  # otherwise get the matrix, caclulate the inverse and store it in
+  # the cache
+  data <- x$getMatrix()
+  inverse <- solve(data)
+  y$cacheInverse(inverse)
+  
+  # return the inverse
+  inverse
 }
