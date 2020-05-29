04b399dea6b63b30edb2501ac9eeda995ddfcfe8
prachi-natu <prachinatuproject@gmail.com>
2020-05-29 14:35:54 +0530

    Initial commit Assignment 2

--- ProgrammingAssignment2-master/cachematrix.R
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -1,44 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-  cache <- NULL
-  setMatrix <- function(newValue) {
-    x <<- newValue
-    cache <<- NULL
-
-  }
-  getMatrix <- function() {
-    x
-  }
-  cacheInverse <- function(solve) {
-    cache <<- solve
-  }
-  getInverse <- function() {
-    cache
-  }
-  list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
-}
+library(datasets)
+data("iris")
+?iris
+Sepal.lengt
+install.packages("data.table")
+library(data.table)
+iris_dt <- as.data.table(iris)
+iris_dt[Species == "virginica",round(mean(Sepal.Length)) ]
+m<-apply(iris[,1:4],2,mean)
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  inverse <- x$getInverse()
-  # if a cached value exists return it
-  if(!is.null(inverse)) {
-    message("getting cached data")
-    return(inverse)
-  }
-  # otherwise get the matrix, caclulate the inverse and store it in
-  # the cache
-  data <- x$getMatrix()
-  inverse <- solve(data)
-  y$cacheInverse(inverse)
-  
-  # return the inverse
-  inverse
-}
